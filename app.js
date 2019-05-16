384934f2e24e70c95d467da62913dda6f1367bcd
Mohamed M. Abd Elhafiz <mabdelhafiz@deemalab.com>
2019-05-16 03:37:41 +0200

    implemented examples for url path params and optional query params

--- app.js
+++ app.js
@@ -2,21 +2,28 @@ const express = require("express");
 const app = express();
 const PORT_NUM = 3001;
 
-app.get("/", handleRequest);
-app.get("/login", handleRequest);
-app.get("/users/:name/", handleRequest);
+app.get("/", handleHomeRequest);
+app.get("/login", handleLoginRequest);
+app.get("/profile/:id", handleProfileRequest);
+app.get("/reg", handleRegRequest);
 
 app.listen(PORT_NUM);
 print("server is listening on port " + PORT_NUM);
 
-function handleRequest(request, response) {
-  if (request.url == "/") {
-    response.send("Landing Page");
-  } else if (request.url == "/login") {
-    response.send("Login Page");
-  } else {
-    print(request.params);
-  }
+function handleHomeRequest(request, response) {
+  response.send("Landing Page");
+}
+
+function handleLoginRequest(request, response) {
+  response.send("Login Page");
+}
+
+function handleProfileRequest(request, response) {
+  response.send(request.params);
+}
+
+function handleRegRequest(request, response) {
+  response.send(request.query);
 }
 
 function print(msg) {
